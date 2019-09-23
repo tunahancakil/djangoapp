b5d3f5379033391a480f2348118110de8b38104f
Tunahan Çakıl <tcakil@kocfinans.com.tr>
2019-09-23 12:14:04 +0300

    Ön yüz için db objeler eklendi.

--- home/views.py
+++ home/views.py
@@ -3,16 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from anket.models import Sorular
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from anket.soruform import SorularForm
 
 # Create your views here.
 def home_view(request):
     sorularım = Sorular.objects.all()
-    soruform = SorularForm()
     if request.user.is_authenticated:
         context = {
             'isim' : 'Tuna',
-            'soru_baslik' : soruform,
             'sorularim' : sorularım,
         }
     else:
