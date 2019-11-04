ad7e04293c9f6519870d3d2a28f8079e9743d9a1
Tunahan Çakıl <tcakil@kocfinans.com.tr>
2019-11-04 16:30:35 +0300

    migrate test

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,11 +13,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import os
 import django_heroku
 
-PORT = 8080
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
@@ -81,6 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'CONN_MAX_AGE':500,
     }
 }
 
