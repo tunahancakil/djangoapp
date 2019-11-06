c5bf3edbac502204f2aec36f38ec202f63832109
Tunahan Çakıl <tcakil@kocfinans.com.tr>
2019-11-06 16:14:20 +0300

    setting.py dir[]

--- mysite/settings.py
+++ mysite/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': (os.path.join(BASE_DIR,'template')),
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
