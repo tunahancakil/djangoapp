9d509863cb0b48169ee9495f3ff3bc728d10c39b
Tunahan Çakıl <tcakil@kocfinans.com.tr>
2019-12-19 02:32:01 +0300

    sms-email

--- anketgonder/admin.py
+++ anketgonder/admin.py
@@ -6,13 +6,15 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import sys
+from django.http import HttpResponse
 
 
 @admin.register(Anket)
 class AnketGonderAdmin(admin.ModelAdmin):
 
-    list_display = ['anket_adi','islem_tarihi','kullanici_adi','smsbutton']
+    list_display = ['id','anket_adi','islem_tarihi','kullanici_adi','buttons']
     list_filter = ['islem_tarihi']
+    anket_id = Anket.objects.all()
 
     def get_urls(self):
         urls = super().get_urls()
@@ -23,7 +25,7 @@ class AnketGonderAdmin(admin.ModelAdmin):
                 name='mail',
             ),
             path(
-                '/sms_success',
+                'sms_success/',
                 self.process_sms,
                 name='sms',
             ),
@@ -31,7 +33,7 @@ class AnketGonderAdmin(admin.ModelAdmin):
         return custom_urls + urls
 
 
-    def smsbutton(self,event=None):
+    def buttons(self,event=None):
         return format_html(
             '<a class="button" href="{}">Mail Gönder</a>&nbsp;'
             '<a class="button" href="{}">Sms Gönder</a>',
@@ -41,12 +43,13 @@ class AnketGonderAdmin(admin.ModelAdmin):
    
     
 
-    def process_sms(self, request, **kwargs):
+    def process_sms(self, request,*args, **kwargs):
         message= MIMEMultipart()   
         message["From"] = "info@ttyazilim.net"  #Mail'i gönderen kişi
         message["To"] = "tunahancakil@gmail.com"    #Mail'i alan kişi
         message["Subject"] = "Python Smtp ile Mail Gönderme" #Mail'in konusu
         body= """
+
         Python üzerinde smtp modülü
         kullanarak mail gönderiyorum.
         """   #Mail içerisinde yazacak içerik
@@ -54,13 +57,13 @@ class AnketGonderAdmin(admin.ModelAdmin):
         message.attach(body_text)
         #Gmail serverlerine bağlanma işlemi.
         print('here1')
-        server = smtplib.SMTP("smtp.ttyazilim.net",587)  
-        server.connect()
+        server = smtplib.SMTP()  
+        server.connect("smtp.ttyazilim.net",587)
         print('here2')
         server.login("info@ttyazilim.net","Tolgahan123+")
-        server.set_debuglevel(false)
         try:
             server.sendmail(message["From"],message["To"],message.as_string())
         finally:
             print('quit')
             server.quit()
+        return HttpResponse('Thanks')
--- anketgonder/forms.py
+++ anketgonder/forms.py
@@ -1,7 +1,21 @@
 from django import forms
+import json
+import requests
 
 class ContactForm(forms.Form):
 
     from_email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea)
+    url = 'http://sms.corvass.net/json'
+    myobj = {"Authentication": {           
+             "apikey": "4775500361",           
+             "apisecret": "0n6hu04dyiz23xyh9m6m"       },
+             "message": "TUNIS",
+             "msisdnArray": ["5318985507", "05393239896", "905455860993"],
+             "originator": "TUNAHNCAKIL",
+             "senddate": "",       
+             "tags": ["deneme", "tayfun", "tunahan", "MERT"],
+             "description": ""}
+    x = requests.post(url, data = json.dumps(myobj))
+    print(x)
